2471ae06744348d53493e3037350f450c65d8843
AkashWadsan <wadsanakash@gmail.com>
2026-08-07 10:52:14 +0530

    task 30 - while Loop Forward

---
+++ WhileLoopForward.java
@@ -0,0 +1,11 @@
+class WhileLoopForward{
+	public static void main(String args[]){
+		int[] x = {10,20,30,40,50};
+		
+		int i = 0;
+		while(i<x.length){
+			System.out.println(x[i]);
+			i++;
+		}
+	}
+}
